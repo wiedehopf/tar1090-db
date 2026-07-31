0eb3deee1e70488f64d33208e65158379083507e
Matthias Wirth <matthias.wirth@gmail.com>
2026-07-31 19:51:41 +0000

    update URL for mictronics DB
    fixes: https://github.com/wiedehopf/tar1090-db/issues/18

--- update.sh
+++ update.sh
@@ -19,7 +19,7 @@ if ! [[ -d "$(pwd)/longnames/individual-types" ]]; then
 fi
 
 wget -O newTypes.json --compression=auto https://raw.githubusercontent.com/Mictronics/readsb-protobuf/dev/webapp/src/db/types.json
-wget -O mic-db.zip https://www.mictronics.de/aircraft-database/indexedDB_old.php
+wget -O mic-db.zip https://github.com/Mictronics/aircraft-database/raw/refs/heads/main/indexedDB_old.zip
 unzip -o mic-db.zip
 
 wget -O basic-ac-db.json.gz https://downloads.adsbexchange.com/downloads/basic-ac-db.json.gz
